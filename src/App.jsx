15931140866a697d59ad15a0faa3ec17bfb6d303
deepakbaghel18 <db691612@gmail.com>
2026-08-04 01:39:20 +0530

    Fix admin route

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
-import Admin from "./admin/Admin";
 import ProductsPage from "./pages/ProductsPage";
 import ContactPage from "./pages/ContactPage";
+import Admin from "./admin/Admin";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+
         <Route path="/products" element={<ProductsPage />} />
+
         <Route path="/contact" element={<ContactPage />} />
+
         <Route path="/admin" element={<Admin />} />
       </Routes>
     </BrowserRouter>
